b7625f6e9330d26c3066ca52187f358aff98807d
JeRiKo1 <github@lars.fricke.name>
2017-07-03 15:45:16 +0200

    Add files via upload

---
+++ extdep/json-rpc-cpp.cmake
@@ -0,0 +1,20 @@
+# json-rpc-cpp is under heavy development, not yet stable, and multiplatform builds are not yet available. 
+# DO NOT MESS WITH THESE SETTINGS! IF YOU HAVE TO MAKE CHANGES HERE, CONSULT sven@ethdev.com BEFOREHAND!!
+
+if(APPLE)
+	set(CONFIG_CMD cmake -DCMAKE_INSTALL_PREFIX=${ETH_DEPENDENCY_INSTALL_DIR} -DCMAKE_MODULE_PATH:PATH=${CMAKE_CURRENT_SOURCE_DIR} -DETH_DEPENDENCY_INSTALL_DIR:PATH=${ETH_DEPENDENCY_INSTALL_DIR} -DCMAKE_BUILD_TYPE=None -DCMAKE_FIND_FRAMEWORK=LAST -Wno-dev .)
+else()
+    set(CONFIG_CMD cmake -DCMAKE_INSTALL_PREFIX=${ETH_DEPENDENCY_INSTALL_DIR} -DCMAKE_MODULE_PATH:PATH=${CMAKE_CURRENT_SOURCE_DIR} -DETH_DEPENDENCY_INSTALL_DIR:PATH=${ETH_DEPENDENCY_INSTALL_DIR} -DCMAKE_BUILD_TYPE=None -DCMAKE_FIND_FRAMEWORK=LAST .)
+endif()
+
+
+# DO NOT CHANGE ANYTHING HERE!
+ExternalProject_Add(json-rpc-cpp
+    # DEPENDS curl # re-enable later, when we build curl again
+	GIT_REPOSITORY https://github.com/cinemast/libjson-rpc-cpp.git 
+	GIT_TAG v0.3.2
+	BINARY_DIR json-rpc-cpp-prefix/src/json-rpc-cpp
+    CONFIGURE_COMMAND ${CONFIG_CMD}
+	BUILD_COMMAND make -j 3
+	INSTALL_COMMAND make install
+)
